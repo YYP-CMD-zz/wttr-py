e75fba859551154a2e1530be70e3272fdebee812
yyp <yyp.official0001@gmail.com>
2021-09-10 13:37:52 +0200

    Update wttr_py.py

--- wttr_py.py
+++ wttr_py.py
@@ -2,6 +2,9 @@ import urllib.request
 import json
 from urllib.request import urlopen, Request
 import urllib.request
+from tkinter import *
+
+
 
 urlData = "https://wttr.in/Detroit?format=j1"
 webURL = urllib.request.urlopen(urlData)
@@ -14,24 +17,36 @@ j = json.loads(data.decode(encoding))
 LOC = (j["nearest_area"][0]["areaName"][0]["value"])
 VIS = (j["weather"][0]["hourly"][0]["visibilityMiles"] + " Miles")
 SUN = (j["weather"][0]["astronomy"][0]["sunrise"])
+CLOUD = (j["current_condition"][0]["cloudcover"])
+CHS = (j["weather"][0]["hourly"][0]["chanceofsnow"])
+
+
+def fact (DEFAUL):
+
+    TEMPLATE ="{:17}".format("| Location: ") + "{:<17}".format("| " +  DEFAUL) + ("|")
+    return TEMPLATE
+
+
 
 
 #formatting
-LOC_format = "{:15}".format("| Location:: ") + "{:<15}".format("| " + LOC) + ("|")
-VIS_format ="{:15}".format("| Visibilty: ") + "{:<15}".format("| " + VIS) + ("|")
-SUN_format ="{:15}".format("| Sunrise: ") + "{:<15}".format("| " + SUN) + ("|")
-
-
+LOC_format = fact(LOC)
+VIS_format = fact(VIS + " Miles")
+SUN_format = fact(SUN + " AM")
+CLOUD_format = fact(CLOUD + " %")
+CHS_format = fact(CHS + " %")
 
 
 
 #format lines
-test ="------------------------------"
+line ="-----------------------------------"
 
 #Printing area
 
-print(test)
+print(line)
 print(LOC_format)
 print(SUN_format)
 print(VIS_format)
-print(test)
+print(CLOUD_format)
+print(CHS_format)
+print(line)
